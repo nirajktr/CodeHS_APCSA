e2db68cfdca6fbdeae380c37dbe8fc013630db59
Niraj Khatri <nirajkhatri0601@gmail.com>
2024-08-28 13:43:22 +0000

    2.10.8 Race test

---
+++ Unit 2/2.10/2.10.8 Racing/RaceMain.java
@@ -0,0 +1,25 @@
+public class RaceMain
+{
+    public static void main(String[] args)
+    {
+        // Length of the course in meters
+        double distance = 2414; // ~ 1.5 miles
+        
+        // Generate a random acceleration for each car
+        double acceleration1 = 1 + Math.random() * 9; // Random acceleration between 1 and 10
+        double acceleration2 = 1 + Math.random() * 9; // Random acceleration between 1 and 10
+        
+        // Create two Racecar objects
+        Racecar car1 = new Racecar("Driver1", acceleration1);
+        Racecar car2 = new Racecar("Driver2", acceleration2);
+        
+        // Compute the finishing times for both cars
+        double time1 = car1.computeTime(distance);
+        double time2 = car2.computeTime(distance);
+        
+        // Print times of each car
+        System.out.println("First car finished in " + time1 + " seconds");
+        System.out.println("Second car finished in " + time2 + " seconds");
+        
+    }
+}
