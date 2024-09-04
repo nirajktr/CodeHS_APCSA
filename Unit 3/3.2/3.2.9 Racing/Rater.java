9ad6c2030e8d8eb39b621ecbb4341c56287820f5
Niraj Khatri <nirajkhatri0601@gmail.com>
2024-09-04 17:06:28 -0400

    Update Rater.java

--- Unit 3/3.2/3.2.9 Racing/Rater.java
+++ Unit 3/3.2/3.2.9 Racing/Rater.java
@@ -1,39 +1,39 @@
 public class Rater
 {
-    private String name; // name of company
-    private int rating; // number rating (1 - 100)
-    
-    
-    public Rater(String company, int initialRating)
+    private String name;
+    private int rating;
+
+    public Rater(String name, int rating)
     {
-        name = company;
-        rating = initialRating;
+        this.name = name;
+        this.rating = rating;
     }
-    
-    // Set rating to newRating
-    // As long as it's no more than 5
-    public void setRating(int newRating)
-    {
-        if (newRating <= 5) {
-            rating = newRating;
-        }
-    }
-    
-    // Returns the rating of the company
+
     public int getRating()
     {
         return rating;
     }
-    
-    // Returns a string representation of the company
-    // based on their ratings
+
+    public void setRating(int newRating)
+    {
+        if (newRating <= 5)
+        {
+            rating = newRating;
+        }
+    }
+
     public String toString()
     {
-        if (rating < 50) {
+        if (rating < 50)
+        {
             return "Not Recommended Company " + name;
-        } else if (rating > 85) {
+        }
+        else if (rating > 85)
+        {
             return "Gold Star Company " + name;
-        } else {
+        }
+        else
+        {
             return "Well Rated Company " + name;
         }
     }
