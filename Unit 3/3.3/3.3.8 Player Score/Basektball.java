3b4158beae80c8f06075fa7264a7424f29bcce8d
Niraj Khatri <nirajkhatri0601@gmail.com>
2024-09-05 14:45:06 +0000

    fix

--- Unit 3/3.3/3.3.8 Player Score/Basektball.java
+++ Unit 3/3.3/3.3.8 Player Score/Basektball.java
@@ -8,21 +8,27 @@ public class Basketball
         
         // Start by listing the steps you need to take
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the player one's name: ");
-        String playerOne = scanner.nextLine(); 
-        
-
-        System.out.print("Enter the player two's name: ");
-        String playerTwo = scanner.nextLine();
-
-        System.out.print("Enter the player one's score: ");
+        // Prompt for player one's name and score
+        System.out.print("Enter player one's name: ");
+        String playerOne = scanner.nextLine();
+        System.out.print("Enter " + playerOne + "'s score: ");
         int scoreOne = scanner.nextInt();
+        scanner.nextLine(); // Consume newline
 
-        System.out.print("Enter the player two's score: ");
+        // Prompt for player two's name and score
+        System.out.print("Enter player two's name: ");
+        String playerTwo = scanner.nextLine();
+        System.out.print("Enter " + playerTwo + "'s score: ");
         int scoreTwo = scanner.nextInt();
 
-        System.out.println(playerOne + " scored " + scoreOne + " points");
-        System.out.println(playerTwo + " scored " + scoreTwo + " points");
+        // Print scores in alphabetical order
+        if (playerOne.compareTo(playerTwo) < 0) {
+            System.out.println(playerOne + " scored " + scoreOne + " points");
+            System.out.println(playerTwo + " scored " + scoreTwo + " points");
+        } else {
+            System.out.println(playerTwo + " scored " + scoreTwo + " points");
+            System.out.println(playerOne + " scored " + scoreOne + " points");
+        }
 
         if (scoreOne > scoreTwo) {
             System.out.println(playerOne + " wins!");
